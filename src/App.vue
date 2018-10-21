51c1f17c7b1b15c431821deb324b9c1ab66b2b82
Maksim Chernyh <maxmagic894@gmail.com>
2018-10-21 10:58:26 +1000

    Time count

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="time">
+        <p>{{ timeText }}</p>
+        <button @click="setTime">Узнать время</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'timer',
+    data () {
+        return {
+            timeText: ""
+        }
+    },
+    methods:{
+        setTime(){
+            var timeSQL = new Date(2017, 8, 20, 9, 30, 39);
+            var newTime = new Date();
+            if(timeSQL.getFullYear() == newTime.getFullYear()){
+                if(timeSQL.getMonth() == newTime.getMonth()){
+                    if(timeSQL.getDate() == newTime.getDate()){
+                        if(timeSQL.getHours() == newTime.getHours()){
+                            if((newTime.getMinutes()-timeSQL.getMinutes())<10){
+                               this.timeText="Online"
+                            }
+                            else{
+                                this.timeText="Был в сети "+(newTime.getMinutes()-timeSQL.getMinutes())+" минут назад"
+                            }
+                        }
+                        else{
+                            this.timeText="Был в сети "+(newTime.getHours()-timeSQL.getHours())+" часов(а) назад"
+                        }
+                    }
+                    else{
+                        if((newTime.getDate()-timeSQL.getDate())==1){
+                            this.timeText="Был в сети вчера"
+                        }
+                        else{
+                            this.timeText="Был в сети "+timeSQL.getDate()+" "+timeSQL.getMonth()
+                        }
+                    }
+                }
+                else{
+                    this.timeText="Был в сети "+timeSQL.getDate()+" "+timeSQL.getMonth()
+                }
+            }
+            else{
+                this.timeText="Был в сети "+timeSQL.getDate()+" "+timeSQL.getMonth()+" "+timeSQL.getFullYear()+" ,"+timeSQL.getHours()+":"+timeSQL.getMinutes()
+            }
+        }        
+    },
+    computed:{
+        
+    }
+}
+</script>
+
+<style>
+    
+</style>
