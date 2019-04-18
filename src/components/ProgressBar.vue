3f7d4b1781926e37d4d368beec38793240c66bfb
Maksim Chernyh <maxmagic894@gmail.com>
2019-04-18 22:13:51 +1000

    Add ProgressBar component

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="progress">
-        <canvas v-insert-message="{ maxProgress: maxProgress, loadProcess: loadProcess }"></canvas>
+    <div class="set-size charts-container">
+
+      <div class="progress-wrapper progress-45">
+        <div class="progress">
+          <div class="left-side half-circle"></div>
+          <div class="right-side half-circle"></div>
+        </div>
+        <div class="shadow"></div>
+      </div>
+
     </div>
 </template>
 
@@ -13,37 +21,99 @@
                 
             }
         },
-        directives: {
-            insertMessage: function(canvasElement, binding) {
-                var ctx = canvasElement.getContext("2d");
-                var s = JSON.stringify
-                var maxProgress = s(binding.value.maxProgress);
-                var loadProcess = s(binding.value.loadProcess);
-                var step = 100/maxProgress;
-                ctx.strokeStyle='#dee9ed';
-                ctx.lineWidth=10;
-                ctx.beginPath();
-                ctx.arc(55, 55, 50, 0, step*maxProgress, false);
-                ctx.stroke();
-                ctx.closePath();
-
-                ctx.strokeStyle='#2ebbe8';
-                ctx.beginPath();
-                ctx.arc(55, 55, 50, 0, step*0.063*loadProcess, false);
-                ctx.stroke();
-                ctx.closePath();
+        watch: {
+            'loadProcess': {
+                handler: function () {
+                    
+                },
+                immediate: true
             }
         }
     }
 </script>
 
 <style scoped lang="less">
-    .progress{
-        width: 110px;
-        height: 110px;
-        transform: rotate(-90deg);
+    // Vars
+    @default-size: 1em;
+    @maxVol: 5;
+    @step: 100/@maxVol;
+    @progressVol: 2;
+
+    // Mixins
+    .size(@width, @height) {
+      height: @height;
+      width: @width;
+    }
+
+    .draw-progress(@progress, @color) {
+      .progress {
+        .half-circle {
+          border-color: @color;
+        }
+
+        .left-side {
+          transform: rotate(@progress * 3.6deg);
+        }
+
+        .right-side when(@progress <= 50){
+            display: none;
+        }
+        .right-side when(@progress > 50) {
+            transform: rotate(180deg);
+        }
+      }
+      .progress when(@progress > 50){
+        clip: rect(auto, auto, auto, auto);
+      }
+    }
+
+    // Selectors
+    *,
+    *:before,
+    *:after {
+      box-sizing: border-box;
+    }
+
+    .set-size {
+      font-size: 10em;
+    }
+
+    .progress-wrapper {
+      .size(@default-size, @default-size);
+      float: left;
+      margin: 15px;
+      position: relative;
+
+      &:nth-child(3n + 1) {
+        clear: both;
+      }
+
+      .progress {
+        .size(100%, 100%);
+        clip: rect(0, @default-size, @default-size, @default-size / 2);
+        left: 0;
         position: absolute;
-        left: calc(~"50% - 55px");
-        top: calc(~"50% - 55px");
+        top: 0;
+
+        .half-circle {
+          .size(100%, 100%);
+          border: (@default-size / 10) solid #3498db;
+          border-radius: 50%;
+          clip: rect(0, @default-size / 2, @default-size, 0);
+          left: 0;
+          position: absolute;
+          top: 0;
+        }
+      }
+
+      .shadow {
+        .size(100%, 100%);
+        border: @default-size / 10 solid #bdc3c7;
+        border-radius: 50%;
+      }
+
+      &.progress-45 {
+        .draw-progress(@progressVol*@step, #1abc9c);
+      }
     }
 </style>
